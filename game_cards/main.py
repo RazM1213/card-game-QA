e34bd2f64643438c5d5e20e1772ae3af6df0c876
Raz Matzliah <razm1213@gmail.com>
2022-01-11 00:18:53 +0200

    added main game loop logic to main.py module

--- game_cards/main.py
+++ game_cards/main.py
@@ -12,3 +12,19 @@ game = CardGame(player1, player2, num_of_cards)
 print(f"Each player has {num_of_cards} cards... ")
 print(f"{game.player1.name}'s cards: {game.player1.cards}")
 print(f"{game.player2.name}'s cards: {game.player2.cards}\n")
+
+# MAIN GAME LOOP:
+for i in range(1, 11):
+    print(f"=====ROUND {i}=====")
+    player1_card = game.player1.get_card()
+    player2_card = game.player2.get_card()
+    print(f"{game.player1} threw {player1_card} to the table !")
+    print(f"{game.player2} threw {player2_card} to the table !")
+    if player1_card > player2_card:
+        game.player1.cards.append(player1_card)
+        game.player1.cards.append(player2_card)
+        print(f"{game.player1.name} WINS !\n")
+    else:
+        game.player2.cards.append(player1_card)
+        game.player2.cards.append(player2_card)
+        print(f"{game.player2.name} WINS !\n")
