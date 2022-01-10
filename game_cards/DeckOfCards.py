b25c14ea3bf42d4b68340b9716d5375305138b55
Raz Matzliah <razm1213@gmail.com>
2022-01-10 23:48:00 +0200

    Initialized DeckOfCards class

--- game_cards/DeckOfCards.py
+++ game_cards/DeckOfCards.py
@@ -1,2 +1,12 @@
 from Card import Card
 import random
+
+
+class DeckOfCards:
+    # creating a deck of card with all the cards values of every suit
+    def __init__(self):
+        self.deck = []
+        for suit in range(1, 5):
+            for value in range(1, 14):
+                self.deck.append(Card(value, suit))
+                
