941ca754352d5cbb525920d4780bd8a0fc7101b9
Raz Matzliah <razm1213@gmail.com>
2022-01-11 00:14:58 +0200

    added game initialization logic to main.py module

--- game_cards/main.py
+++ game_cards/main.py
@@ -1,1 +1,10 @@
 from CardGame import CardGame
+
+player1 = input("Enter player 1 name: ")
+player2 = input("Enter player 2 name: ")
+num_of_cards = int(input("How many cards to hand to each player? "))
+
+game = CardGame(player1, player2, num_of_cards)
+print(f"Each player has {num_of_cards} cards... ")
+print(f"{game.player1.name}'s cards: {game.player1.cards}")
+print(f"{game.player2.name}'s cards: {game.player2.cards}\n")
