2e4ba2eb1fd575138b838640b40a0f9e88db49e0
Raz Matzliah <razm1213@gmail.com>
2022-01-10 23:42:17 +0200

    added get_name method to Card class

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -11,3 +11,34 @@ class Card:
             raise ValueError("suit must be in range 1-4 !")
         self.value = value
         self.suit = suit
+
+    # A method to convert the card object attributes to text - for the card representation to be user-friendly
+    def get_name(self):
+        """
+        The method uses two dictionaries - to create the string representation of the card.
+        One is for the card value
+        The second is for the card suit
+        """
+        value_dict = {
+            1: "Ace",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            11: "Jack",
+            12: "Queen",
+            13: "King"
+        }
+
+        suit_dict = {
+            1: "Diamond",
+            2: "Spade",
+            3: "Heart",
+            4: "Club"
+        }
+        return f"{value_dict[self.value]} Of {suit_dict[self.suit]}"
