22792c8b4bc9f1f6f259a5bfed0cd958d50cb2e3
Raz Matzliah <razm1213@gmail.com>
2022-01-10 23:43:27 +0200

    added __gt__ method to Card class

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -45,3 +45,21 @@ class Card:
 
     def __repr__(self):
         return self.get_name()
+
+    # This method used to compare two cards objects:
+    def __gt__(self, card):
+        """
+        Cards are compared by their value - higher value = stronger card
+        If the compared cards share the same value - the comparison will be based on their suits
+        Ace is the strongest card in the game - it's value is the highest amongst cards
+        """
+        if type(card) != Card:
+            raise TypeError("card type must be of Card !")
+        if self.value > card.value != 1:
+            return True
+        elif self.value == card.value:
+            if self.suit > card.suit:
+                return True
+        elif self.value == 1 and card.value != 1:
+            return True
+        return False
