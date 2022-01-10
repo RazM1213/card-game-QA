2e7d8e3cfb49124904c2876cf43f146f8457209f
Raz Matzliah <razm1213@gmail.com>
2022-01-10 23:56:43 +0200

    added set_hand method to Player class

--- game_cards/Player.py
+++ game_cards/Player.py
@@ -20,3 +20,17 @@ class Player:
     def __str__(self):
         return f"{self.name}"
 
+    # Used to hand out cards to the player out of the deck of cards:
+    def set_hand(self, deck_of_cards: DeckOfCards):
+        """
+        This method gets DeckOfCards object as a parameter, and deals the player the amount of cards it has to get
+        Each card is unique
+        """
+        if type(deck_of_cards) != DeckOfCards:
+            raise TypeError("Deck of cards must be a DeckOfCards type!")
+        if len(deck_of_cards.deck) < self.num_of_cards:
+            raise ValueError("There's not enough cards in the deck to hand out to the player")
+        for i in range(self.num_of_cards):
+            card = deck_of_cards.deal_one()
+            self.cards.append(card)
+
