615d01bbc8f7b46ea325d8cae0afab255308aa24
Raz Matzliah <razm1213@gmail.com>
2022-01-10 23:50:19 +0200

    added deal_one method in DeckOfCards class

--- game_cards/DeckOfCards.py
+++ game_cards/DeckOfCards.py
@@ -9,8 +9,15 @@ class DeckOfCards:
         for suit in range(1, 5):
             for value in range(1, 14):
                 self.deck.append(Card(value, suit))
-    
+
     # A method that shuffles the cards randomly in the cards deck
     def card_shuffle(self):
         """shuffle the cards"""
         random.shuffle(self.deck)
+
+    # Pick one cards from the deck
+    def deal_one(self):
+        """picking random card from the deck"""
+        card = random.choice(self.deck)
+        self.deck.remove(card)
+        return card
