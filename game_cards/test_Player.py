6966d47b8ce0cf358ca773c3275a44dfce1efc8f
Raz Matzliah <razm1213@gmail.com>
2022-01-11 12:16:29 +0200

    Created test_Player.py module for Player class unit-testing

---
+++ game_cards/test_Player.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Player import Player
+
+
+class TestPlayer(TestCase):
+    def setUp(self):
+        pass
+
+    def test__init__(self):
+        pass
+    
+    def test_set_hand(self):
+        self.fail()
+
+    def test_get_card(self):
+        self.fail()
+
+    def test_add_card(self):
+        self.fail()
