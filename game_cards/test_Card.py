36a732465f9e4ca59f51b130f74fb3afcb16ea6f
Raz Matzliah <razm1213@gmail.com>
2022-01-11 09:38:05 +0200

    created unit-test module to Card class

---
+++ game_cards/test_Card.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from Card import Card
+
+
+class TestCard(TestCase):
+    def test_get_name(self):
+        pass
+
+    def test__init__(self):
+        pass
+
+    def test__gt__(self):
+        pass
