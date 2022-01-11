aa1ec50979984f23e18f8bbe50c2b28ec6ab27d5
Itamar lebel <itamarlebel1@gmail.com>
2022-01-11 09:41:42 +0200

    create test_DeckOfCards

---
+++ game_cards/test_DeckOfCards.py
@@ -0,0 +1,17 @@
+from unittest import TestCase, mock
+from DeckOfCards import DeckOfCards
+from unittest.mock import patch
+
+
+class TestDeckOfCards(TestCase):
+    def setUp(self):
+        pass
+
+    def test__init__(self):
+        pass
+
+    def test_card_shuffle(self):
+        self.fail()
+
+    def test_deal_one(self):
+        self.fail()
