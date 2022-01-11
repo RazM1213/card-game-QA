38b29f43068f90dbc4fe1357ed56b29ca0f0660c
Itamar lebel <itamarlebel1@gmail.com>
2022-01-11 09:29:34 +0200

    Edited card_shuffle and deal_one method in DeckOfCards

--- game_cards/DeckOfCards.py
+++ game_cards/DeckOfCards.py
@@ -16,11 +16,15 @@ class DeckOfCards:
     # A method that shuffles the cards randomly in the cards deck
     def card_shuffle(self):
         """shuffle the cards"""
+        if len(self.deck) < 2:
+            raise AttributeError("Can't shuffle when there's less than 2 cards!")
         random.shuffle(self.deck)
 
     # Pick one cards from the deck
     def deal_one(self):
         """picking random card from the deck"""
+        if len(self.deck) == 0:
+            raise AttributeError("Cant pick card from empty deck!")
         card = random.choice(self.deck)
         self.deck.remove(card)
         return card
